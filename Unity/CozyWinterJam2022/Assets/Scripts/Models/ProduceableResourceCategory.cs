78ef0d749610ffe161175fec332c78f03554311b
Kevin <kasmeltz@lakeheadu.ca>
2022-12-02 12:46:09 -0500

    multiple resource types

--- Unity/CozyWinterJam2022/Assets/Scripts/Models/ProduceableResourceCategory.cs
+++ Unity/CozyWinterJam2022/Assets/Scripts/Models/ProduceableResourceCategory.cs
@@ -3,6 +3,9 @@
     public enum ProduceableResourceCategory
     {
         Wood = 0,
-        Chocolate = 1
+        Coal = 1,
+        Food = 2,
+        Cookies = 3,
+        SimplePresent = 4
     }
 }
