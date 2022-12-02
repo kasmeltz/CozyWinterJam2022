28527542227553c13fe1e0c6432782a8384ce833
Kevin <kasmeltz@lakeheadu.ca>
2022-12-02 01:19:36 -0500

    resources over time!

---
+++ Unity/CozyWinterJam2022/Assets/Scripts/Behaviours/ResourceAmountTextBehaviour.cs
@@ -0,0 +1,39 @@
+namespace HNS.CozyWinterJam2022.Behaviours
+{
+    using HNS.CozyWinterJam2022.Models;
+    using TMPro;
+    using UnityEngine;
+
+    [AddComponentMenu("CWJ2022/ResourceAmountText")]
+
+    public class ResourceAmountTextBehaviour : MonoBehaviour
+    {
+        #region Members
+
+        public TMP_Text TextObject;
+        public ProduceableResourceCategory ProduceableResourceCategory;
+
+        protected GameworldBehaviour Gameworld { get; set;}
+
+        #endregion
+
+        #region Methods
+
+        protected void Update()
+        {
+            int index = (int)ProduceableResourceCategory;
+
+            var amount = Gameworld.Inventory[index];
+
+            TextObject.text = amount
+                .ToString("0.##");
+        }
+
+        protected void Awake()
+        {
+            Gameworld = FindObjectOfType<GameworldBehaviour>();
+        }
+
+        #endregion
+    }
+}
