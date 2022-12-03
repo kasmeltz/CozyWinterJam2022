7952f2001fcc359be1116aec51ba53bd57849041
Kevin <kasmeltz@lakeheadu.ca>
2022-12-02 19:34:50 -0500

    new building , resource and worker types

--- Unity/CozyWinterJam2022/Assets/Scripts/Models/ProduceableResourceCategory.cs
+++ Unity/CozyWinterJam2022/Assets/Scripts/Models/ProduceableResourceCategory.cs
@@ -3,9 +3,14 @@
     public enum ProduceableResourceCategory
     {
         Wood = 0,
-        Coal = 1,
-        Food = 2,
-        Cookies = 3,
-        SimplePresent = 4
+        Food = 1,
+        Gingerbread = 2,
+        Coal = 3,        
+        Cookies = 4,
+        Steel = 5,
+        Present1 = 6,
+        Present2 = 7,
+        Present3 = 8,
+        Present4 = 9
     }
 }
